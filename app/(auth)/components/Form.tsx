5c9c661f424f2451aec984e762f562710277c30d
reypjaidev <reypjaidev@gmail.com>
2026-07-14 00:21:05 +0800

    chore: move bg component to layout

--- app/(auth)/components/Form.tsx
+++ app/(auth)/components/Form.tsx
@@ -62,10 +62,6 @@ const FormContainer = ({
         {isLogin ? "Don't have an account?" : "Already have an account?"}
         <span className="text-indigo-400 hover:underline ml-1">click here</span>
       </Link>
-      <div className="fixed inset-0 -z-1 pointer-events-none">
-        <div className="absolute left-1/2 top-20 -translate-x-1/2 w-245 h-115 bg-linear-to-tr from-indigo-800/35 to-transparent rounded-full blur-3xl" />
-        <div className="absolute right-12 bottom-10 w-105 h-55 bg-linear-to-bl from-indigo-700/35 to-transparent rounded-full blur-2xl" />
-      </div>
     </form>
   );
 };
