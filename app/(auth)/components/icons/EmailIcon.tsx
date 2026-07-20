50397ce53ec73d9f9930afc0eb1d2161be784fab
reypjaidev <reypjaidev@gmail.com>
2026-07-20 23:34:13 +0800

    fix: update email icon svg

--- app/(auth)/components/icons/EmailIcon.tsx
+++ app/(auth)/components/icons/EmailIcon.tsx
@@ -2,18 +2,19 @@ function EmailIcon() {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      width="16"
-      height="16"
+      width="14"
+      height="14"
       viewBox="0 0 24 24"
       fill="none"
       stroke="currentColor"
-      className="text-white/60"
+      className="text-gray-400"
       strokeWidth="2"
       strokeLinecap="round"
       strokeLinejoin="round"
     >
       {" "}
-      <circle cx="12" cy="8" r="5" /> <path d="M20 21a8 8 0 0 0-16 0" />{" "}
+      <path d="m22 7-8.991 5.727a2 2 0 0 1-2.009 0L2 7" />{" "}
+      <rect x="2" y="4" width="20" height="16" rx="2" />{" "}
     </svg>
   );
 }
